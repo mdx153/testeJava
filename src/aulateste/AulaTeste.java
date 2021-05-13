d0ed72488eb1a37bf2397c800a1b91b3dafe92e8
Maycon Douglas <etherius789@gmail.com>
2021-05-12 21:48:28 -0300

    Adicionando projeto

---
+++ src/aulateste/AulaTeste.java
@@ -0,0 +1,24 @@
+
+package aulateste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AulaTeste {
+
+   
+    public static void main(String[] args) {
+        
+        List<Integer> numeros = new ArrayList<>();
+        
+        for (int i = 0; i < 10; i++) {
+            numeros.add(i);
+            System.out.println("Número atual: " + numeros);
+            
+        }
+        
+        
+    }
+    
+}
